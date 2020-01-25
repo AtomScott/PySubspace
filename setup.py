c636e94b792af4b64df55c3f5b9b26c75570e1e3
Atom Scott <atom.james.scott@gmail.com>
2020-01-25 19:15:33 +0900

    change to manual package finding

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='cvt',
     version='0.1.0',
-    packages=find_packages(),
+    packages=['cvt'],
     install_requires=[
         'numpy',
         'scikit-learn',
